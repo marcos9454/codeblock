3741c6f3e6014ab55884896de7bc42c99accfa5e
gatsbaby.876@gmail.com <gatsbaby.876@gmail.com>
2020-01-12 10:58:25 +0530

    commit for all and one

--- Project/routes/tasks.js
+++ Project/routes/tasks.js
@@ -1,10 +1,12 @@
 var express = require('express');
-
 var router = express.Router();
-var mongojs = require('mongodb').MongoClient;
-var url="mongodb://localhost:27017/";
 
-router.get('/tasks',function(req, res, next){
+var mongojs = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var url="mongodb://localhost:27017";
+
+// GET ALL
+router.get('/tasks/all',function(req, res, next){
 	mongojs.connect(url	,function(err,out){
 		if(err)
 			res.send(err);
@@ -15,6 +17,23 @@ router.get('/tasks',function(req, res, next){
 			if(err)
 				res.send(err);
 			res.send(result);
+		});
+	});
+});
+
+//GET ONE
+router.get('/tasks/one/:id',function(req, res, next){
+	mongojs.connect(url	,function(err,out){
+		if(err)
+			res.send(err);
+
+		var dbs = out.db('cron');
+
+		dbs.collection("user").find({_id:ObjectId('5d9a2a0687d4dd24781c3b91')}, function (err,result){
+			if(err)
+				res.send(err);
+			res.send('nudes');
+			res.send(result.name);
 		});
 	});
 });
