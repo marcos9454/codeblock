cf66b87097e9ef02b29615224555cd329badf411
marcos <gatsbaby.876@gmail.com>
2020-01-14 00:31:18 +0530

    work.js construct

--- Project/server.js
+++ Project/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var tasks = require('./routes/tasks');
+var work = require('./routes/work');
 
 var port=3000;
 
@@ -24,6 +25,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/', index);
 app.use('/api', tasks);
+app.use('/work', work);
 
 app.listen(port, function(){
 	console.log('Server started on port '+port);
